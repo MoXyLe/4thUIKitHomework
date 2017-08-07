b0adffea1f859896b5168092cbf075588aaafc84
Виктор <viktor@MacBook-Pro-Viktor.local>
2017-08-08 00:49:15 +0700

    Improved code and added comments

--- UIHomework4/CustomisingElements.swift
+++ UIHomework4/CustomisingElements.swift
@@ -6,11 +6,12 @@
 //  Copyright © 2017 Виктор. All rights reserved.
 //
 
-import Foundation
 import UIKit
 
 func customiseLabel(label: UILabel, x: Int, y: Int, text: String) {
+    
     let vc = UIViewController()
+    
     label.backgroundColor = UIColor.lightGray.withAlphaComponent(0.80)
     label.frame = CGRect(x: x, y: y, width: Int(vc.view.center.x + 20), height: Int(vc.view.bounds.height / 5) + 15)
     label.numberOfLines = 6
@@ -18,10 +19,13 @@ func customiseLabel(label: UILabel, x: Int, y: Int, text: String) {
     label.font = UIFont.systemFont(ofSize: 20)
     label.adjustsFontSizeToFitWidth = true
     label.text = text
+    
 }
 
 func customiseSwitch(UIswitch: UISwitch, x: Int, y: Int) {
+    
     UIswitch.frame = CGRect(x: x, y: y, width: 0, height: 0)
     UIswitch.tintColor = UIColor.red
     UIswitch.onTintColor = UIColor.blue
+    
 }
--- UIHomework4/OOOViewController.swift
+++ UIHomework4/OOOViewController.swift
@@ -19,28 +19,43 @@ class OOOViewController: UIViewController {
     let thirdSwitch = UISwitch()
     let priceLabel = UILabel()
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-    
-    // Не советую сюда заходить, опасно
-    
     override func viewWillLayoutSubviews() {
+        
         let xSwitchPosition = Int(self.view.center.x + 30 + (self.view.center.x - 81) / 2)
-        image.frame = self.view.frame
+        // Common x position switch
+        
+        image.frame = self.view.frame   //Positioning image on the screen
+        
         customiseLabel(label: firstLabel, x: 10, y: Int(self.view.center.y) / 4, text: "Подготовка и подача документов на регистрацию лично заявителем")
+        // Positioning and changing firstLabel
+        
         customiseSwitch(UIswitch: firstSwitch, x: xSwitchPosition , y: 55 + Int(firstLabel.bounds.height) / 2)
+        // Positioning and changing firstSwitch
+        
         firstSwitch.addTarget(self, action: #selector(firstSelector(_:)), for: .valueChanged)
-        firstSwitch.setOn(true, animated: false)
+        // Setting off other switches and sending text to the priceLabel, when turning on
+        
+        firstSwitch.setOn(true, animated: false)    // Turning switch on
+        
         customiseLabel(label: secondLabel, x: 10, y: Int(firstLabel.bounds.height) + Int(self.view.center.y) / 4 + 20, text: "Самостоятельная подготовка документов и направление почтой или через представителя")
+        
         customiseSwitch(UIswitch: secondSwitch, x: xSwitchPosition, y: 75 + Int(firstLabel.bounds.height) * 3 / 2)
+        
         secondSwitch.addTarget(self, action: #selector(secondSelector(_:)), for: .valueChanged)
+        
         customiseLabel(label: thirdLabel, x: 10, y: Int(secondLabel.bounds.height) + Int(firstLabel.bounds.height) + Int(self.view.center.y) / 4 + 40, text: "Подготовка и подача на регистрацию специализированной фирмой")
+        
         customiseSwitch(UIswitch: thirdSwitch, x: xSwitchPosition, y: 95 + Int(firstLabel.bounds.height) * 5 / 2)
+        
         thirdSwitch.addTarget(self, action: #selector(thirdSelector(_:)), for: .valueChanged)
+        
         priceLabel.backgroundColor = UIColor.lightGray.withAlphaComponent(0.80)
+        
         priceLabel.frame = CGRect(x: 10, y: Int(thirdLabel.bounds.height + secondLabel.bounds.height + firstLabel.bounds.height) + Int(self.view.center.y) / 4 + 60, width: (Int(self.view.center.x) * 2) - 20, height: 40)
+        
         priceLabel.text = "     Стоимость: 4000 рублей"
+        // Customising and positioning priceLabel
+        
         self.view.addSubview(image)
         self.view.addSubview(firstSwitch)
         self.view.addSubview(firstLabel)
@@ -49,54 +64,65 @@ class OOOViewController: UIViewController {
         self.view.addSubview(thirdLabel)
         self.view.addSubview(thirdSwitch)
         self.view.addSubview(priceLabel)
+        // Adding all the elements to the view
+        
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
+    // Setting off other switches and sending text to the priceLabel, when turning on
     func firstSelector(_ selfSwitch: UISwitch) {
+        
         if selfSwitch.isOn {
+            
             secondSwitch.setOn(false, animated: true)
             thirdSwitch.setOn(false, animated: true)
             priceLabel.text = "     Стоимость: 4000 Рублей"
+            
         }
+        
         if !selfSwitch.isOn && !secondSwitch.isOn && !thirdSwitch.isOn{
+            
             priceLabel.text = ""
+            
         }
+        
     }
     
+    // Setting off other switches and sending text to the priceLabel, when turning on
     func secondSelector(_ selfSwitch: UISwitch) {
+        
         if selfSwitch.isOn {
+            
             firstSwitch.setOn(false, animated: true)
             thirdSwitch.setOn(false, animated: true)
             priceLabel.text = "     Стоимость: 7700 Рублей"
+            
         }
+        
         if !selfSwitch.isOn && !firstSwitch.isOn && !thirdSwitch.isOn{
+            
             priceLabel.text = ""
+            
         }
+        
     }
     
+    // Setting off other switches and sending text to the priceLabel, when turning on
     func thirdSelector(_ selfSwitch: UISwitch) {
+        
         if selfSwitch.isOn {
+            
             secondSwitch.setOn(false, animated: true)
             firstSwitch.setOn(false, animated: true)
             priceLabel.text = "     Стоимость: 9900 Рублей"
+            
         }
+        
         if !selfSwitch.isOn && !secondSwitch.isOn && !firstSwitch.isOn{
+            
             priceLabel.text = ""
+            
         }
+        
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
